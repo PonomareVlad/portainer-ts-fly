802cbe36e0cd4ca63f410e129a0a52f6881b8604
Vladislav Ponomarev <2877584+PonomareVlad@users.noreply.github.com>
2026-02-09 05:21:46 +0100

    Enhance start.sh with network setup and Tailscale options
    Added network configuration and modified Tailscale commands.

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-/app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-/app/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=fly-app
-/app/my-app
+modprobe xt_mark
+
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p /etc/sysctl.conf
+
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+/app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --verbose=1 --port 41641 &
+/app/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=portainer --advertise-exit-node --ssh
+/app/tailscale serve --bg 9000
+
+exec /portainer --http-enabled --bind "127.0.0.1:9000" --bind-https ""
